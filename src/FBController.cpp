7e3114729d6c4811eed81cf32b57bd9b66c1dd8b
yuki <yukiyukiyu@github.com>
2018-07-04 16:53:21 +0800

    Add ReadFileToBuf, GetBIDByFOff

--- src/FBController.cpp
+++ src/FBController.cpp
@@ -1,3 +1,4 @@
+#include<iostream>
 #include "FBController.h"
 #include "BSFSParams.h"
 #include "VHDController.h"
@@ -31,10 +32,26 @@ bool FBController::SaveSuperBlock()
 
 bool FBController::Recycle(int blockID)
 {
+    if (this->superblock.cnt == GROUPSIZE)
+    {
+        if(!this->vhdc.WriteBlock(blockID, (char*)&this->superblock, sizeof(SuperBlock)))
+            return false;
+        memset((char*)&this->superblock, 0, sizeof(SuperBlock));
+        this->superblock.cnt = 1;
+    }
+    else this->superblock.cnt++;
+    this->superblock.freeStack[this->superblock.cnt-1] = blockID;
     return true;
 }
 
 bool FBController::Distribute(int* blockID)
 {
+    if(this->superblock.freeStack[this->superblock.cnt-1] == 0) return false;
+    *blockID = this->superblock.freeStack[this->superblock.cnt-1];
+    if(this->superblock.cnt == 1)
+    {
+        if(!this->vhdc.ReadBlock(*blockID, (char*)&this->superblock, sizeof(SuperBlock)))
+            return false;
+    }
     return true;
 }
